34f93f9b55a05e04b8a0eb4fd160d5038df68cd5
eatekha <atekhaeseosa@gmail.com>
2023-10-16 19:26:38 -0400

    Demo Button

--- components/hero.tsx
+++ components/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Welcome to MyFinance, a data visualization tool to simplify financial tracking, and guaranteed to save you time!</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="/signup">Join Today</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://my-finance-dashboard-29227dab4c63.herokuapp.com/?user_id=2">Demo App</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="https://github.com/eatekha/MyFinance#readme" target="_blank">Learn more</a>
